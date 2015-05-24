c06d15772b0cfbb37142c80c9b7f8b4b49cecd7b
Robert Scott Dionne <robertsdionne@gmail.com>
2015-05-23 20:29:52 -0400

    Organize by types of convolution.

--- sacred/math.hpp
+++ sacred/math.hpp
@@ -15,7 +15,26 @@ namespace sacred {
 
     virtual ~Math() = default;
 
-    void BackwardConvolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
+    void NarrowConvolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
+        const T output_coefficient, const T input_coefficient) {
+      CHECK_STATE(input.shape(0) - filter.shape(0) + 1 == output.shape(0));
+      CHECK_STATE(input.shape(1) - filter.shape(1) + 1 == output.shape(1));
+      for (auto i = 0; i < output.shape(0); ++i) {
+        for (auto j = 0; j < output.shape(1); ++j) {
+          auto I = i + filter.shape(0) - 1;
+          auto J = j + filter.shape(1) - 1;
+          T current_output = output_coefficient * output.at({i, j});
+          for (auto k = 0; k < filter.shape(0); ++k) {
+            for (auto l = 0; l < filter.shape(1); ++l) {
+              current_output += input_coefficient * filter.at({k, l}) * input.at({I - k, J - l});
+            }
+          }
+          output.at({i, j}) = current_output;
+        }
+      }
+    }
+
+    void BackwardNarrowConvolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
         const T output_coefficient, const T input_coefficient) {
       CHECK_STATE(input.shape(0) - filter.shape(0) + 1 == output.shape(0));
       CHECK_STATE(input.shape(1) - filter.shape(1) + 1 == output.shape(1));
@@ -32,18 +51,18 @@ namespace sacred {
       }
     }
 
-    void BackwardRecurrentConvolve2(Array<T> &output, const Array<T> &filter,
+    void WideConvolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
         const T output_coefficient, const T input_coefficient) {
-      for (auto j = output.shape(1) - 1; j >= 0; --j) {
-        for (auto i = 0; i < output.shape(0); ++i) {
-          auto I = i - filter.shape(0) / 2;
-          auto J = j + 1;
+      CHECK_STATE(input.shape(0) + filter.shape(0) - 1 == output.shape(0));
+      CHECK_STATE(input.shape(1) + filter.shape(1) - 1 == output.shape(1));
+      for (auto i = 0; i < output.shape(0); ++i) {
+        for (auto j = 0; j < output.shape(1); ++j) {
           T current_output = output_coefficient * output.at({i, j});
           for (auto k = 0; k < filter.shape(0); ++k) {
             for (auto l = 0; l < filter.shape(1); ++l) {
-              auto in = 0 <= I + k && I + k < output.shape(0) && 0 <= J + l && J + l < output.shape(1);
+              auto in = 0 <= i - k && i - k < input.shape(0) && 0 <= j - l && j - l < input.shape(1);
               if (in) {
-                current_output += input_coefficient * filter.at({k, l}) * output.at({I + k, J + l});
+                current_output += input_coefficient * filter.at({k, l}) * input.at({i - k, j - l});
               }
             }
           }
@@ -74,18 +93,39 @@ namespace sacred {
       }
     }
 
-    void Convolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
+    void RecurrentConvolve2(Array<T> &output, const Array<T> &filter,
         const T output_coefficient, const T input_coefficient) {
-      CHECK_STATE(input.shape(0) - filter.shape(0) + 1 == output.shape(0));
-      CHECK_STATE(input.shape(1) - filter.shape(1) + 1 == output.shape(1));
-      for (auto i = 0; i < output.shape(0); ++i) {
-        for (auto j = 0; j < output.shape(1); ++j) {
-          auto I = i + filter.shape(0) - 1;
-          auto J = j + filter.shape(1) - 1;
+      for (auto j = 0; j < output.shape(1); ++j) {
+        for (auto i = 0; i < output.shape(0); ++i) {
+          auto I = i + filter.shape(0) / 2;
+          auto J = j - 1;
           T current_output = output_coefficient * output.at({i, j});
           for (auto k = 0; k < filter.shape(0); ++k) {
             for (auto l = 0; l < filter.shape(1); ++l) {
-              current_output += input_coefficient * filter.at({k, l}) * input.at({I - k, J - l});
+              auto in = 0 <= I - k && I - k < output.shape(0) && 0 <= J - l && J - l < output.shape(1);
+              if (in) {
+                current_output += input_coefficient * filter.at({k, l}) * output.at({I - k, J - l});
+              }
+            }
+          }
+          output.at({i, j}) = current_output;
+        }
+      }
+    }
+
+    void BackwardRecurrentConvolve2(Array<T> &output, const Array<T> &filter,
+        const T output_coefficient, const T input_coefficient) {
+      for (auto j = output.shape(1) - 1; j >= 0; --j) {
+        for (auto i = 0; i < output.shape(0); ++i) {
+          auto I = i - filter.shape(0) / 2;
+          auto J = j + 1;
+          T current_output = output_coefficient * output.at({i, j});
+          for (auto k = 0; k < filter.shape(0); ++k) {
+            for (auto l = 0; l < filter.shape(1); ++l) {
+              auto in = 0 <= I + k && I + k < output.shape(0) && 0 <= J + l && J + l < output.shape(1);
+              if (in) {
+                current_output += input_coefficient * filter.at({k, l}) * output.at({I + k, J + l});
+              }
             }
           }
           output.at({i, j}) = current_output;
@@ -108,47 +148,7 @@ namespace sacred {
         }
       }
     }
-
-    void RecurrentConvolve2(Array<T> &output, const Array<T> &filter,
-        const T output_coefficient, const T input_coefficient) {
-      for (auto j = 0; j < output.shape(1); ++j) {
-        for (auto i = 0; i < output.shape(0); ++i) {
-          auto I = i + filter.shape(0) / 2;
-          auto J = j - 1;
-          T current_output = output_coefficient * output.at({i, j});
-          for (auto k = 0; k < filter.shape(0); ++k) {
-            for (auto l = 0; l < filter.shape(1); ++l) {
-              auto in = 0 <= I - k && I - k < output.shape(0) && 0 <= J - l && J - l < output.shape(1);
-              if (in) {
-                current_output += input_coefficient * filter.at({k, l}) * output.at({I - k, J - l});
-              }
-            }
-          }
-          output.at({i, j}) = current_output;
-        }
-      }
-    }
-
-    void WideConvolve2(Array<T> &output, const Array<T> &filter, const Array<T> &input,
-        const T output_coefficient, const T input_coefficient) {
-      CHECK_STATE(input.shape(0) + filter.shape(0) - 1 == output.shape(0));
-      CHECK_STATE(input.shape(1) + filter.shape(1) - 1 == output.shape(1));
-      for (auto i = 0; i < output.shape(0); ++i) {
-        for (auto j = 0; j < output.shape(1); ++j) {
-          T current_output = output_coefficient * output.at({i, j});
-          for (auto k = 0; k < filter.shape(0); ++k) {
-            for (auto l = 0; l < filter.shape(1); ++l) {
-              auto in = 0 <= i - k && i - k < input.shape(0) && 0 <= j - l && j - l < input.shape(1);
-              if (in) {
-                current_output += input_coefficient * filter.at({k, l}) * input.at({i - k, j - l});
-              }
-            }
-          }
-          output.at({i, j}) = current_output;
-        }
-      }
-    }
-};
+  };
 
 }  // namespace sacred
 
--- sacred/math_test.cpp
+++ sacred/math_test.cpp
@@ -8,93 +8,7 @@ using sacred::Math;
 
 auto math = Math<float>();
 
-TEST(MathTest, BackwardConvolve2) {
-  auto a = Array<float>({3, 3}, {
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9
-  });
-  auto b = Array<float>({4, 4}, {
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 16
-  });
-  auto c = Array<float>({2, 2}, {
-    0, 0,
-    0, 0
-  });
-
-  math.BackwardConvolve2(c, a, b, 0.0, 1.0);
-
-  EXPECT_EQ(
-      Array<float>({2, 2}, {
-        348, 393,
-        528, 573
-      }),
-      c);
-}
-
-TEST(MathTest, BackwardRecurrentConvolve2) {
-  auto a = Array<float>({3, 3}, {
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9
-  });
-  auto c = Array<float>({4, 4}, {
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 16
-  });
-
-  math.BackwardRecurrentConvolve2(c, a, 1.0, 1.0);
-
-  EXPECT_EQ(
-      Array<float>({4, 4}, {
-        20490, 1275, 75, 4,
-        23467, 1986, 127, 8,
-        15515, 1694, 179, 12,
-        5296, 661, 91, 16
-      }),
-      c);
-}
-TEST(MathTest, BackwardWideConvolve2) {
-  auto a = Array<float>({3, 3}, {
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9
-  });
-  auto b = Array<float>({4, 4}, {
-    1, 2, 3, 4,
-    5, 6, 7, 8,
-    9, 10, 11, 12,
-    13, 14, 15, 16
-  });
-  auto c = Array<float>({6, 6}, {
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0
-  });
-
-  math.BackwardWideConvolve2(c, a, b, 0.0, 1.0);
-
-  EXPECT_EQ(
-      Array<float>({6, 6}, {
-        9, 26, 50, 74, 53, 28,
-        51, 111, 178, 217, 145, 72,
-        114, 231, 348, 393, 252, 120,
-        186, 363, 528, 573, 360, 168,
-        105, 197, 274, 295, 175, 76,
-        39, 68, 86, 92, 47, 16
-      }),
-      c);
-}
-
-TEST(MathTest, Convolve2) {
+TEST(MathTest, NarrowConvolve2) {
   auto a = Array<float>({3, 3}, {
     9, 8, 7,
     6, 5, 4,
@@ -111,7 +25,7 @@ TEST(MathTest, Convolve2) {
     0, 0
   });
 
-  math.Convolve2(c, a, b, 0.0, 1.0);
+  math.NarrowConvolve2(c, a, b, 0.0, 1.0);
 
   EXPECT_EQ(
       Array<float>({2, 2}, {
@@ -121,57 +35,29 @@ TEST(MathTest, Convolve2) {
       c);
 }
 
-TEST(MathTest, GeneralMatrixMultiplication) {
-  auto a = Array<float>({4, 2}, {
-    1, 2,
-    3, 4,
-    5, 6,
-    7, 8
-  });
-  auto b = Array<float>({2, 3}, {
-    1, 2, 3,
-    4, 5, 6
-  });
-  auto c = Array<float>({4, 3}, {
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0
-  });
-
-  math.GeneralMatrixMultiplication(c, a, b, 0.0, 1.0);
-
-  EXPECT_EQ(
-      Array<float>({4, 3}, {
-        9, 12, 15,
-        19, 26, 33,
-        29, 40, 51,
-        39, 54, 69
-      }),
-      c);
-}
-
-TEST(MathTest, RecurrentConvolve2) {
+TEST(MathTest, BackwardNarrowConvolve2) {
   auto a = Array<float>({3, 3}, {
-    9, 8, 7,
-    6, 5, 4,
-    3, 2, 1
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
   });
-  auto c = Array<float>({4, 4}, {
+  auto b = Array<float>({4, 4}, {
     1, 2, 3, 4,
     5, 6, 7, 8,
     9, 10, 11, 12,
     13, 14, 15, 16
   });
+  auto c = Array<float>({2, 2}, {
+    0, 0,
+    0, 0
+  });
 
-  math.RecurrentConvolve2(c, a, 1.0, 1.0);
+  math.BackwardNarrowConvolve2(c, a, b, 0.0, 1.0);
 
   EXPECT_EQ(
-      Array<float>({4, 4}, {
-        1, 53, 1446, 34685,
-        5, 120, 2749, 48191,
-        9, 196, 2777, 39825,
-        13, 119, 1400, 17795
+      Array<float>({2, 2}, {
+        348, 393,
+        528, 573
       }),
       c);
 }
@@ -207,6 +93,121 @@ TEST(MathTest, WideConvolve2) {
         186, 363, 528, 573, 360, 168,
         105, 197, 274, 295, 175, 76,
         39, 68, 86, 92, 47, 16
+      }),
+      c);
+}
+
+TEST(MathTest, BackwardWideConvolve2) {
+  auto a = Array<float>({3, 3}, {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+  });
+  auto b = Array<float>({4, 4}, {
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16
+  });
+  auto c = Array<float>({6, 6}, {
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0
+  });
+
+  math.BackwardWideConvolve2(c, a, b, 0.0, 1.0);
+
+  EXPECT_EQ(
+      Array<float>({6, 6}, {
+        9, 26, 50, 74, 53, 28,
+        51, 111, 178, 217, 145, 72,
+        114, 231, 348, 393, 252, 120,
+        186, 363, 528, 573, 360, 168,
+        105, 197, 274, 295, 175, 76,
+        39, 68, 86, 92, 47, 16
+      }),
+      c);
+}
+
+TEST(MathTest, RecurrentConvolve2) {
+  auto a = Array<float>({3, 3}, {
+    9, 8, 7,
+    6, 5, 4,
+    3, 2, 1
+  });
+  auto c = Array<float>({4, 4}, {
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16
+  });
+
+  math.RecurrentConvolve2(c, a, 1.0, 1.0);
+
+  EXPECT_EQ(
+      Array<float>({4, 4}, {
+        1, 53, 1446, 34685,
+        5, 120, 2749, 48191,
+        9, 196, 2777, 39825,
+        13, 119, 1400, 17795
+      }),
+      c);
+}
+
+TEST(MathTest, BackwardRecurrentConvolve2) {
+  auto a = Array<float>({3, 3}, {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+  });
+  auto c = Array<float>({4, 4}, {
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16
+  });
+
+  math.BackwardRecurrentConvolve2(c, a, 1.0, 1.0);
+
+  EXPECT_EQ(
+      Array<float>({4, 4}, {
+        20490, 1275, 75, 4,
+        23467, 1986, 127, 8,
+        15515, 1694, 179, 12,
+        5296, 661, 91, 16
+      }),
+      c);
+}
+
+TEST(MathTest, GeneralMatrixMultiplication) {
+  auto a = Array<float>({4, 2}, {
+    1, 2,
+    3, 4,
+    5, 6,
+    7, 8
+  });
+  auto b = Array<float>({2, 3}, {
+    1, 2, 3,
+    4, 5, 6
+  });
+  auto c = Array<float>({4, 3}, {
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+  });
+
+  math.GeneralMatrixMultiplication(c, a, b, 0.0, 1.0);
+
+  EXPECT_EQ(
+      Array<float>({4, 3}, {
+        9, 12, 15,
+        19, 26, 33,
+        29, 40, 51,
+        39, 54, 69
       }),
       c);
 }
