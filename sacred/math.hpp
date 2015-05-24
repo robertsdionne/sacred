d4f1a2ea125e4da7e2cb55b50b9c1e35b13f14ee
Robert Scott Dionne <robertsdionne@gmail.com>
2015-05-23 20:27:48 -0400

    Fix BackwardRecurrentConvolve2.

--- sacred/math.hpp
+++ sacred/math.hpp
@@ -34,14 +34,14 @@ namespace sacred {
 
     void BackwardRecurrentConvolve2(Array<T> &output, const Array<T> &filter,
         const T output_coefficient, const T input_coefficient) {
-      for (auto j = 0; j < output.shape(1); ++j) {
+      for (auto j = output.shape(1) - 1; j >= 0; --j) {
         for (auto i = 0; i < output.shape(0); ++i) {
           auto I = i - filter.shape(0) / 2;
-          auto J = j - filter.shape(1);
+          auto J = j + 1;
           T current_output = output_coefficient * output.at({i, j});
           for (auto k = 0; k < filter.shape(0); ++k) {
             for (auto l = 0; l < filter.shape(1); ++l) {
-              auto in = 0 <= I + k && I + k < output.shape(0) && 0 <= J + l;
+              auto in = 0 <= I + k && I + k < output.shape(0) && 0 <= J + l && J + l < output.shape(1);
               if (in) {
                 current_output += input_coefficient * filter.at({k, l}) * output.at({I + k, J + l});
               }
@@ -118,7 +118,7 @@ namespace sacred {
           T current_output = output_coefficient * output.at({i, j});
           for (auto k = 0; k < filter.shape(0); ++k) {
             for (auto l = 0; l < filter.shape(1); ++l) {
-              auto in = 0 <= I - k && I - k < output.shape(0) && 0 <= J - l;
+              auto in = 0 <= I - k && I - k < output.shape(0) && 0 <= J - l && J - l < output.shape(1);
               if (in) {
                 current_output += input_coefficient * filter.at({k, l}) * output.at({I - k, J - l});
               }
