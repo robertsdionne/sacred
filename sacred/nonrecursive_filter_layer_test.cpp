d2bffa0019d4cc6cbd3b2a0efa61a34b5fa30f2e
Robert Scott Dionne <robertsdionne@gmail.com>
2015-05-24 02:02:31 -0400

    Add unit test for filter and bias gradients.

--- sacred/nonrecursive_filter_layer_test.cpp
+++ sacred/nonrecursive_filter_layer_test.cpp
@@ -91,6 +91,7 @@ TEST(NonrecursiveFilterLayer, Gradient) {
   auto layer = NonrecursiveFilterLayer<float>(bias, filter);
 
   constexpr auto kEpsilon = 1e-3f;
+
   for (auto i = 0; i < input.shape(0); ++i) {
     for (auto j = 0; j < input.shape(1); ++j) {
       auto output = Blob<float>({2, 2});
@@ -135,6 +136,102 @@ TEST(NonrecursiveFilterLayer, Gradient) {
 
       auto expected_partial_error_with_respect_to_input_ij = (loss_1 - loss_0) / (2.0f * kEpsilon);
       EXPECT_NEAR(expected_partial_error_with_respect_to_input_ij, actual_partial_error_with_respect_to_input_ij, 1e-1);
+    }
+  }
+
+  for (auto i = 0; i < filter.shape(0); ++i) {
+    for (auto j = 0; j < filter.shape(1); ++j) {
+      auto output = Blob<float>({2, 2});
+      input.diff() = Array<float>({4, 4});
+      bias.diff() = Array<float>({2, 2});
+      filter.diff() = Array<float>({3, 3});
+      layer.Forward(input, &output);
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          output.diff({k, l}) = -delta;
+        }
+      }
+      layer.Backward(output, &input);
+
+      auto actual_partial_error_with_respect_to_filter_ij = filter.diff({i, j});
+      auto original_filter_ij = filter.value({i, j});
+
+      output = Blob<float>({2, 2});
+      filter.value({i, j}) = original_filter_ij + kEpsilon;
+      layer.Forward(input, &output);
+      auto loss_1 = 0.0f;
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          loss_1 += delta * delta / 2.0f;
+        }
+      }
+
+      output = Blob<float>({2, 2});
+      filter.value({i, j}) = original_filter_ij - kEpsilon;
+      layer.Forward(input, &output);
+      auto loss_0 = 0.0f;
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          loss_0 += delta * delta / 2.0f;
+        }
+      }
+
+      filter.value({i, j}) = original_filter_ij;
+
+      auto expected_partial_error_with_respect_to_filter_ij = (loss_1 - loss_0) / (2.0f * kEpsilon);
+      EXPECT_NEAR(expected_partial_error_with_respect_to_filter_ij,
+          actual_partial_error_with_respect_to_filter_ij, 1e-1);
+    }
+  }
+
+  for (auto i = 0; i < bias.shape(0); ++i) {
+    for (auto j = 0; j < bias.shape(1); ++j) {
+      auto output = Blob<float>({2, 2});
+      input.diff() = Array<float>({4, 4});
+      bias.diff() = Array<float>({2, 2});
+      filter.diff() = Array<float>({3, 3});
+      layer.Forward(input, &output);
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          output.diff({k, l}) = -delta;
+        }
+      }
+      layer.Backward(output, &input);
+
+      auto actual_partial_error_with_respect_to_bias_ij = bias.diff({i, j});
+      auto original_bias_ij = bias.value({i, j});
+
+      output = Blob<float>({2, 2});
+      bias.value({i, j}) = original_bias_ij + kEpsilon;
+      layer.Forward(input, &output);
+      auto loss_1 = 0.0f;
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          loss_1 += delta * delta / 2.0f;
+        }
+      }
+
+      output = Blob<float>({2, 2});
+      bias.value({i, j}) = original_bias_ij - kEpsilon;
+      layer.Forward(input, &output);
+      auto loss_0 = 0.0f;
+      for (auto k = 0; k < output.shape(0); ++k) {
+        for (auto l = 0; l < output.shape(1); ++l) {
+          auto delta = target.value({k, l}) - output.value({k, l});
+          loss_0 += delta * delta / 2.0f;
+        }
+      }
+
+      bias.value({i, j}) = original_bias_ij;
+
+      auto expected_partial_error_with_respect_to_bias_ij = (loss_1 - loss_0) / (2.0f * kEpsilon);
+      EXPECT_NEAR(expected_partial_error_with_respect_to_bias_ij,
+          actual_partial_error_with_respect_to_bias_ij, 1e-1);
     }
   }
 }
