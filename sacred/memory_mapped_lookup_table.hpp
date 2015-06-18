9f0133e491f420775353db0a2b397068e4644a18
Robert Scott Dionne <robertsdionne@gmail.com>
2015-06-17 22:33:56 -0400

    Allow word vector lookup.

--- sacred/memory_mapped_lookup_table.hpp
+++ sacred/memory_mapped_lookup_table.hpp
@@ -3,11 +3,13 @@
 
 #include <cstdio>
 #include <fcntl.h>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <unordered_map>
 
 #include "blob.hpp"
 #include "lookup_table.hpp"
@@ -15,12 +17,21 @@
 namespace sacred {
 
   using std::string;
+  using std::unordered_map;
+  using std::vector;
 
   template <typename F>
   class MemoryMappedLookupTable : public LookupTable<F> {
   public:
-    MemoryMappedLookupTable(const std::string &file_to_map) {
-      auto file_descriptor = open(file_to_map.c_str(), O_RDONLY);
+    MemoryMappedLookupTable(int dimension, const string &index_file, const string &vector_file) : dimension(dimension) {
+      std::ifstream index_in(index_file);
+      for (auto i = 0; index_in.good(); ++i) {
+        string word;
+        index_in >> word;
+        index.insert({word, i});
+        reverse_index.insert({i, word});
+      }
+      auto file_descriptor = open(vector_file.c_str(), O_RDONLY);
       fstat(file_descriptor, &stat_info);
       auto map = mmap(nullptr, stat_info.st_size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_PRIVATE, file_descriptor, 0);
       close(file_descriptor);
@@ -34,6 +45,22 @@ namespace sacred {
       }
     }
 
+    string word_at(int i) {
+      if (i < 0 || i >= reverse_index.size()) {
+        return string();
+      }
+      return reverse_index[i];
+    }
+
+    int index_of(const string &word) {
+      auto result = index.find(word);
+      if (index.cend() != result) {
+        return result->second;
+      } else {
+        return -1;
+      }
+    }
+
     F at(int index) const {
       if (index < 0 || index >= stat_info.st_size / sizeof(F)) {
         return F(0.0);
@@ -42,12 +69,24 @@ namespace sacred {
     }
 
     void Forward(const vector<string> &bottom, Blob<F> *top) override {
+      auto j = 0;
+      for (auto &word : bottom) {
+        auto word_index = index.at(word);
+        for (auto i = 0; i < 300; ++i) {
+          top->value().set({i, j}, at(word_index * dimension + i));
+        }
+        ++j;
+      }
     }
 
     void Backward(const Blob<F> &top, const vector<string> &bottom) override {
+      // TODO: Make this optional instead of unsupported.
     }
 
   private:
+    int dimension;
+    unordered_map<string, int> index;
+    unordered_map<int, string> reverse_index;
     struct stat stat_info;
     F *memory_map;
   };
--- sacred/sacred.cpp
+++ sacred/sacred.cpp
@@ -14,30 +14,27 @@ using std::ifstream;
 using std::numeric_limits;
 
 DEFINE_string(input_filename, u8"", u8"Test");
+DEFINE_int32(lookup_table_vector_dimension, 300, u8"The word vector dimension.");
 DEFINE_string(lookup_table_vectors, u8"data/large/vectors.dat", u8"The word vector lookup table data file.");
 DEFINE_string(lookup_table_vocabulary, u8"data/large/vocabulary.txt", u8"The word vocabulary lookup table data file.");
 
 int main(int argument_count, char *arguments[]) {
   gflags::ParseCommandLineFlags(&argument_count, &arguments, true);
 
-  auto lookup_table = MemoryMappedLookupTable<float>(FLAGS_lookup_table_vectors);
-
-  for (auto i = 0; i < 300; ++i) {
-    std::cout << lookup_table.at(i) << u8" ";
-  }
-  std::cout << std::endl;
+  auto lookup_table = MemoryMappedLookupTable<float>(
+      FLAGS_lookup_table_vector_dimension, FLAGS_lookup_table_vocabulary, FLAGS_lookup_table_vectors);
 
   auto input_filename = std::string(FLAGS_input_filename);
 
   ifstream in(input_filename);
 
-  auto input = Blob<float>({1024, 1024});
-  auto bias_0 = Blob<float>({1024 - 32 + 1, 1024 - 32 + 1});
-  auto filter_0 = Blob<float>({32, 32});
-  auto output_0 = Blob<float>({1024 - 32 + 1, 1024 - 32 + 1});
-  auto bias_1 = Blob<float>({1024 - 32 + 1});
-  auto filter_1 = Blob<float>({32, 32});
-  auto output_1 = Blob<float>({1024 - 32 + 1, 1024 - 32 + 1});
+  auto input = Blob<float>({300, 6});
+  auto bias_0 = Blob<float>({300 - 32 + 1, 6 - 3 + 1});
+  auto filter_0 = Blob<float>({32, 3});
+  auto output_0 = Blob<float>({300 - 32 + 1, 6 - 3 + 1});
+  auto bias_1 = Blob<float>({300 - 32 + 1});
+  auto filter_1 = Blob<float>({32, 3});
+  auto output_1 = Blob<float>({300 - 32 + 1, 6 - 3 + 1});
 
   filter_0.value().Randomize();
   filter_1.value().Randomize();
@@ -45,9 +42,8 @@ int main(int argument_count, char *arguments[]) {
   auto nonrecursive = NonrecursiveFilterLayer<float>(bias_0, filter_0);
   auto recursive = RecursiveFilterLayer<float>(bias_1, filter_1);
 
-  std::cout << u8"reading data" << std::endl;
-  in.read(reinterpret_cast<char *>(input.value().data()), 1024 * 1024 * sizeof(float));
-
+  std::cout << u8"lookup_table.Forward()" << std::endl;
+  lookup_table.Forward({"the", "cat", "in", "the", "hat", "sat"}, &input);
   std::cout << u8"nonrecursive.Forward()" << std::endl;
   nonrecursive.Forward(input, &output_0);
   std::cout << u8"recursive.Forward()" << std::endl;
