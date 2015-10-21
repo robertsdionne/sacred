8b4bc7c0dfadfa39d53b1f8d5e359d23b0f3d333
Robert Scott Dionne <robertsdionne@gmail.com>
2015-10-20 20:39:22 -0400

    Add checked_index_test.

---
+++ sacred/checked_index_test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "checked_index.hpp"
+
+namespace sacred {
+
+TEST(CheckedIndex, Transform) {
+  auto checked_index = CheckedIndex<>();
+
+  EXPECT_DEATH(checked_index.Transform({3}, {1}, {-2}).at(0), "index\\.at\\(i\\)");
+  EXPECT_DEATH(checked_index.Transform({3}, {1}, {-1}).at(0), "index\\.at\\(i\\)");
+  EXPECT_EQ(0, checked_index.Transform({3}, {1}, {0}).at(0));
+  EXPECT_EQ(1, checked_index.Transform({3}, {1}, {1}).at(0));
+  EXPECT_EQ(2, checked_index.Transform({3}, {1}, {2}).at(0));
+  EXPECT_DEATH(checked_index.Transform({3}, {1}, {3}).at(0), "index\\.at\\(i\\)");
+  EXPECT_DEATH(checked_index.Transform({3}, {1}, {4}).at(0), "index\\.at\\(i\\)");
+}
+
+}  // namespace sacred
