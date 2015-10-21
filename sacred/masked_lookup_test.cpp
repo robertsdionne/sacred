059a06419df00b369059f62debfd922a95f83592
Robert Scott Dionne <robertsdionne@gmail.com>
2015-10-20 20:28:59 -0400

    Add masked_lookup_test.

---
+++ sacred/masked_lookup_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "masked_lookup.hpp"
+
+namespace sacred {
+
+TEST(MaskedLookup, Lookup) {
+  auto masked_lookup = MaskedLookup<>();
+  auto data = vector<float>({1, 2, 3, 4});
+
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {-1, -1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {-1, 0}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {-1, 1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {-1, 2}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {0, -1}));
+  EXPECT_EQ(1, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {0, 0}));
+  EXPECT_EQ(2, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {0, 1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {0, 2}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {1, -1}));
+  EXPECT_EQ(3, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {1, 0}));
+  EXPECT_EQ(4, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {1, 1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {1, 2}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {2, -1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {2, 0}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {2, 1}));
+  EXPECT_EQ(0, masked_lookup.Lookup(data, data.size(), {2, 2}, {2, 1}, {2, 2}));
+}
+
+}  // namespace sacred
