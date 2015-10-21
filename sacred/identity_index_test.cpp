2e47d5ac0cea6246d72fd3fae885ce6b8a51f51e
Robert Scott Dionne <robertsdionne@gmail.com>
2015-10-20 20:43:05 -0400

    Add identity_index_test.

---
+++ sacred/identity_index_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "identity_index.hpp"
+
+namespace sacred {
+
+TEST(IdentityIndex, Transform) {
+  auto identity_index = IdentityIndex<>();
+
+  EXPECT_EQ(-1, identity_index.Transform({3}, {1}, {-1}).at(0));
+  EXPECT_EQ(0, identity_index.Transform({3}, {1}, {0}).at(0));
+  EXPECT_EQ(1, identity_index.Transform({3}, {1}, {1}).at(0));
+  EXPECT_EQ(2, identity_index.Transform({3}, {1}, {2}).at(0));
+  EXPECT_EQ(3, identity_index.Transform({3}, {1}, {3}).at(0));
+}
+
+}  // namespace sacred
