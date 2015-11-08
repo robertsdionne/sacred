06565471958372e51d1ceaf1b68244f484b44eac
Robert Scott Dionne <robertsdionne@gmail.com>
2015-11-08 10:36:28 -0500

    Add Operator Rectifier.

---
+++ sacred/rectifier.hpp
@@ -0,0 +1,42 @@
+#ifndef SACRED_RECTIFIER_HPP_
+#define SACRED_RECTIFIER_HPP_
+
+#include <algorithm>
+#include <glog/logging.h>
+
+#include "default_types.hpp"
+#include "operator.hpp"
+
+namespace sacred {
+
+template <typename F = default_floating_point_type>
+class Rectifier : public Operator<F> {
+public:
+  using tensor_type = typename default_tensor_type<F>::value;
+  using tensors_type = typename default_tensors_type<F>::value;
+  using tensors_const_type = typename default_tensors_const_type<F>::value;
+
+  Rectifier() = default;
+
+  virtual ~Rectifier() = default;
+
+  void operator ()(const tensor_type &x, tensor_type &y) {
+    using std::max;
+    CHECK_EQ(x.order(), y.order());
+    for (auto i = 0; i < x.shape().at(0); ++i) {
+      for (auto j = 0; j < x.shape().at(1); ++j) {
+        y.set({i, j}, max(F(0), x.at({i, j})));
+      }
+    }
+  }
+
+  virtual void operator ()(const tensors_const_type &in, const tensors_type &out) override {
+    auto x = in.at(0);
+    auto y = out.at(0);
+    operator ()(*x, *y);
+  }
+};
+
+}  // namespace sacred
+
+#endif  // SACRED_RECTIFIER_HPP_
