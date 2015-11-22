68884678fe521243744b651581afe0f211c0f5b2
Robert Scott Dionne <robertsdionne@gmail.com>
2015-11-22 15:56:07 -0500

    Generalize TEST(FullyConnected, Gradient).

--- sacred/fully_connected_test.cpp
+++ sacred/fully_connected_test.cpp
@@ -29,56 +29,54 @@ TEST(FullyConnected, Gradient) {
   using std::tie;
 
   auto input = Tensor<Dual>({4, 1}, {1, 2, 3, 4});
-  auto expected_input_gradient = Tensor<>({4, 1}, {38, 44, 50, 56});
-
   auto bias = Tensor<Dual>({3, 1}, {1, 2, 3});
-  auto expected_bias_gradient = Tensor<>({3, 1}, {1, 2, 3});
-
   auto weight = Tensor<Dual>({3, 4}, {
     1, 2, 3, 4,
     5, 6, 7, 8,
     9, 10, 11, 12,
   });
-  auto expected_weight_gradient = Tensor<>({3, 4}, {
+  auto target = Tensor<Dual>({3, 1}, {30, 70, 110});
+
+  auto input_gradient = Tensor<>({4, 1});
+  auto bias_gradient = Tensor<>({3, 1});
+  auto weight_gradient = Tensor<>({3, 4});
+
+  for (auto pair : {
+    make_pair(&input, &input_gradient),
+    make_pair(&bias, &bias_gradient),
+    make_pair(&weight, &weight_gradient),
+  }) {
+    Tensor<Dual> *parameter;
+    Tensor<> *parameter_gradient;
+    tie(parameter, parameter_gradient) = pair;
+
+    for (auto i = 0; i < parameter->size(); ++i) {
+      auto output = Tensor<Dual>({3, 1}, {0, 0, 0});
+      auto op = FullyConnected<Dual>(bias, weight);
+
+      parameter->data(i) += 1_ɛ;
+
+      op(input, output);
+
+      auto loss = 0_ɛ;
+      for (auto j = 0; j < target.size(); ++j) {
+        auto delta = target.data(j) - output.data(j);
+        loss += delta * delta / 2.0f;
+      }
+
+      parameter_gradient->data(i) = loss.dual;
+
+      parameter->data(i).dual = 0;
+    }
+  }
+
+  EXPECT_EQ(Tensor<>({4, 1}, {38, 44, 50, 56}), input_gradient);
+  EXPECT_EQ(Tensor<>({3, 1}, {1, 2, 3}), bias_gradient);
+  EXPECT_EQ(Tensor<>({3, 4}, {
     1, 2, 3, 4,
     2, 4, 6, 8,
     3, 6, 9, 12,
-  });
-
-  auto target = Tensor<Dual>({3, 1}, {30, 70, 110});
-
-  for (auto pair : {
-    make_pair(&input, &expected_input_gradient),
-    make_pair(&bias, &expected_bias_gradient),
-    make_pair(&weight, &expected_weight_gradient),
-  }) {
-    Tensor<Dual> *parameter;
-    Tensor<> *expected_parameter_gradient;
-    tie(parameter, expected_parameter_gradient) = pair;
-
-    for (auto i = 0; i < parameter->shape().at(0); ++i) {
-      for (auto j = 0; j < parameter->shape().at(1); ++j) {
-        auto output = Tensor<Dual>({3, 1}, {0, 0, 0});
-        auto op = FullyConnected<Dual>(bias, weight);
-
-        parameter->at({i, j}) += 1_ɛ;
-
-        op(input, output);
-
-        auto loss = 0_ɛ;
-        for (auto u = 0; u < target.shape().at(0); ++u) {
-          for (auto v = 0; v < target.shape().at(1); ++v) {
-            auto delta = target.at({u, v}) - output.at({u, v});
-            loss += delta * delta / 2.0f;
-          }
-        }
-
-        EXPECT_EQ(expected_parameter_gradient->at({i, j}), loss.dual);
-
-        parameter->at({i, j}).dual = 0;
-      }
-    }
-  }
+  }), weight_gradient);
 }
 
 }  // namespace sacred
