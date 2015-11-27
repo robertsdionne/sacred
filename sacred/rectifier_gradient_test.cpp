0f9ddfefd2afc136e98561cea69ab1b5e870a76b
Robert Scott Dionne <robertsdionne@gmail.com>
2015-11-26 21:15:25 -0500

    Move TEST(Rectifier, Gradient) to TEST(RectifierGradient, Dual).

--- sacred/rectifier_gradient_test.cpp
+++ sacred/rectifier_gradient_test.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
+#include <utility>
 
+#include "dual.hpp"
+#include "gradients.hpp"
+#include "rectifier.hpp"
 #include "rectifier_gradient.hpp"
 #include "tensor.hpp"
 
@@ -24,6 +28,41 @@ TEST(RectifierGradient, Run) {
   });
 
   op(output_gradient, input, input_gradient);
+
+  EXPECT_EQ(Tensor<>({3, 4}, {
+    1, 0, 1, 0,
+    0, 1, 0, 1,
+    1, 0, 1, 0,
+  }), input_gradient);
+}
+
+TEST(RectifierGradient, Dual) {
+  using std::make_pair;
+
+  auto input = Tensor<Dual>({3, 4}, {
+    1, -2, 3, -4,
+    -5, 6, -7, 8,
+    9, -10, 11, -12,
+  });
+  auto target = Tensor<Dual>({3, 4}, {
+    0, -1, 2, -1,
+    -1, 5, -1, 7,
+    8, -1, 10, -1,
+  });
+
+  auto input_gradient = Tensor<>({3, 4});
+
+  TestGradients<Rectifier<Dual>>({
+    make_pair(&input, &input_gradient),
+  }, [] () {
+    return new Tensor<Dual>({3, 4}, {
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+    });
+  }, [] () {
+    return new Rectifier<Dual>();
+  }, input, target);
 
   EXPECT_EQ(Tensor<>({3, 4}, {
     1, 0, 1, 0,
